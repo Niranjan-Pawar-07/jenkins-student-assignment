9a56eaa720970ee0a7dc255d040ae3a26d76182f
Niranjan-Pawar-07 <niranjanpawar2112@gmail.com>
2026-08-14 10:08:31 +0530

    adding trigger- jenkins

--- src/main/Student.java
+++ src/main/Student.java
@@ -1,5 +1,9 @@
 public class Student {
     public static void main(String[] args) {
         System.out.println("Running Student Application");
+
+        System.out.println("Hello, Jenkins!");
+        System.out.println("This is a simple Java application for Jenkins pipeline demonstration.");
+        System.out.println("Build-info: " + new java.util.Date());
     }
 }
